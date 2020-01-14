cb7e5efcb80b46d622314250971131e6a1e17354
Taku Kamata <taku.kamata@gmail.com>
2020-01-14 12:53:28 +0900

    add the clear Timer.

--- src/MyTimer.tsx
+++ src/MyTimer.tsx
@@ -1,6 +1,7 @@
 import * as React from "react";
 
 export default class MyTimer extends React.Component {
+    timer;
 
     constructor(props) {
         super(props);
@@ -8,13 +9,16 @@ export default class MyTimer extends React.Component {
     }
 
     componentDidMount() {
-        setInterval(() => {
+        this.timer = setInterval(() => {
             this.setState({ cur: new Date() });
         }, 1000);
     }
 
-    render() {
+    componentWillUnmount(){
+        clearInterval(this.timer);
+    }
 
+    render() {
         return <div>現在時刻:{this.state.cur.toLocaleTimeString()}</div>;
     }
 }
